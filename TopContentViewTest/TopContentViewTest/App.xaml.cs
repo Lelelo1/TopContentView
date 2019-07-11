cad0ee7543fb2c15421fdbb5c2048d37a6fa8cc3
Lelelo1 <leowetterek@Leos-Air-2.lan>
2019-07-12 01:41:28 +0200

    created library for nuget creation and xamarin.forms project for testing

---
+++ TopContentViewTest/TopContentViewTest/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using TopContentViewTest.Views;
+
+namespace TopContentViewTest
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            var shell = new AppShell();
+            shell.CurrentItem = new ShellContent() { ContentTemplate = new DataTemplate(() => new TestPage()) };
+            MainPage = shell;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
